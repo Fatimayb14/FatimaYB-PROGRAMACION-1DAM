58d9ceffef83445f7a9290644d9e63b3c430df38
diurno <diurno@equipo-00>
2024-10-08 11:08:31 +0200

    Este es otro intento

---
+++ src/Unidad1/HolaMundo.java
@@ -0,0 +1,12 @@
+package Unidad1;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Primer intento de java
+		System.out.println("Hola Mundo!!");
+	}
+
+}
